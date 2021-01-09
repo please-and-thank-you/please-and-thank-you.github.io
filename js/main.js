c6e8d6ac8461dc4845d5b67fa3df910d05046bf8
Nate <gabriel.nate@gmail.com>
2021-01-08 16:16:30 -0800

    WIP playing around with animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+gsap.registerPlugin(ScrollTrigger);
+
 (function () {
+
+  // Bring Site Header in
+  gsap.to('.site-header', {
+    scrollTrigger: {
+      trigger: '.site-header'
+    },
+    delay: 0.25,
+    y: '0px',
+    alpha: 1,
+    duration: 1
+  });
+
+  // Bring Greeting in
+  gsap.to('.greeting', {
+    scrollTrigger: {
+      trigger: '.greeting'
+    },
+    delay: 0.25,
+    y: '0px',
+    rotate: 0,
+    alpha: 1,
+    duration: 1
+  });
+
+  // Scroll Greeting out
+  gsap.to('.greeting', {
+    scrollTrigger: {
+      trigger: '.section-landing',
+      scrub: true
+    },
+    rotate: -15,
+  });
+
 
 })();
