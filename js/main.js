646e65712316d0c8aebbb2f5efc713dcfbabfc5d
Nate <gabriel.nate@gmail.com>
2021-01-12 12:26:21 -0800

    updating some class names to better reflect contents, adding in scroll animations

--- js/main.js
+++ js/main.js
@@ -3,36 +3,106 @@ gsap.registerPlugin(ScrollTrigger);
 (function () {
 
   // Bring Site Header in
-  gsap.to('.site-header', {
+  gsap.from('.site-header', {
+    delay: 0.25,
+    y: '130px',
+    alpha: 0,
+    duration: 1,
     scrollTrigger: {
       trigger: '.site-header'
-    },
-    delay: 0.25,
-    y: '0px',
-    alpha: 1,
-    duration: 1
+    }
   });
 
   // Bring Greeting in
-  gsap.to('.greeting', {
+  gsap.from('.greeting', {
+    delay: 0.25,
+    y: '130px',
+    rotate: 15,
+    alpha: 0,
+    duration: 1,
     scrollTrigger: {
       trigger: '.greeting'
-    },
-    delay: 0.25,
-    y: '0px',
-    rotate: 0,
-    alpha: 1,
-    duration: 1
+    }
   });
 
   // Scroll Greeting out
-  gsap.to('.greeting', {
-    scrollTrigger: {
-      trigger: '.section-landing',
-      scrub: true
-    },
+  gsap.fromTo('.greeting', {
+    rotate: 0
+  }, {
     rotate: -15,
+    scrollTrigger: {
+      trigger: '.section-hero',
+      toggleActions: 'restart none none none',
+      start: 'top 100%',
+      end: 'top 0%',
+      scrub: true,
+      // markers: true
+    }
   });
 
+  // Fade in Section Hero
+  // gsap.from('.hero-background', {
+  //   opacity: 0,
+  //   scrollTrigger: {
+  //     trigger: '.section-hero',
+  //     toggleActions: 'restart none none none',
+  //     start: 'top 100%',
+  //     end: 'top 0%',
+  //     scrub: true,
+  //     // markers: true
+  //   }
+  // });
+
+  // Fade out Section Hero
+  gsap.to('.hero-background', {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.section-info',
+      toggleActions: 'restart none none none',
+      start: 'top 90%',
+      end: 'top 0%',
+      scrub: true,
+      // markers: true
+    }
+  });
+
+  // Bring in footer logo
+  gsap.from('.section-info .paty-logo', {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.section-info',
+      toggleActions: 'restart none none none',
+      start: 'top 20%',
+      end: 'top 0%',
+      scrub: true,
+      markers: true
+    }
+  });
+
+  // Bring in Contact info background
+  gsap.from('.contact-background', {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.section-info',
+      toggleActions: 'restart none none none',
+      start: 'top 90%',
+      end: 'top 10%',
+      scrub: true,
+      // markers: true
+    }
+  });
+
+  // Bring in Contact email address
+  gsap.from('.contact-info', {
+    rotate: 90,
+    scrollTrigger: {
+      trigger: '.section-info',
+      toggleActions: 'restart none none none',
+      start: 'top 90%',
+      end: 'top 10%',
+      scrub: true,
+      // markers: true
+    }
+  });
 
 })();
